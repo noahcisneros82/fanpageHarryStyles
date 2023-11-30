79a18f048519a7e3d5378b950f57fc0e4cbf00bb
noahcisneros82 <ashtoncisneros81@gmail.com>
2023-11-30 01:02:34 -0600

    tipos de datos

---
+++ js/ej1.js
@@ -0,0 +1,37 @@
+
+console.log("hola mundo");
+
+//Tipos de datos
+
+//String
+let nombre = "Noah";// Cadenas de texto o caracteres
+console.log(nombre);//Imprimir en consola el valor de la variable nombre 
+//Number
+let edad = 20; //Numeros
+console.log(edad);//Imprimir en consola el valor de la variable edad
+
+//Float
+let altura = 1.75; //Numeros con decimales
+
+//Boolean
+let esHumano = true; //Verdadero 
+let esRobot = false; //Falso
+
+//Array
+let colores = ["rojo", "verde", "azul"]; //Lista de datos
+
+//Object
+let persona = {
+    nombre: "Noah",
+    edad: 16,
+    esHumano: true,
+    colores: ["rojo", "verde", "azul"]
+}; //Objeto
+
+
+//Variables
+nombre= "Noa";
+console.log(nombre);
+
+
+//constantes
