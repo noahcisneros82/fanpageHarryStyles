0e08f9f2614d0c354439edf98d880670e5f35e6e
noahcisneros82 <ashtoncisneros81@gmail.com>
2023-11-30 01:02:44 -0600

    ejercicios geniales

---
+++ js/ej2.js
@@ -0,0 +1,26 @@
+//Crear un loop de 1 a 100 que imprima los números pares en la consola
+/*let i = 4;
+for (i; i <= 9; i++){
+   if (i==5){
+      console.log('i es igual a ='+i);
+   } else {
+    console.log("este es el valor de i="+i);
+   }
+
+}*/
+
+//queiro saber cuantos años tenia Noah desde su nacimiento a la fecha.
+
+let fechaNacimiento = 1994;
+let fechaActual = 2023;
+let edad = 0;
+let nombre = "Harry bb";
+
+for (i = fechaNacimiento; i <= fechaActual; i++) {
+    console.log("en el año " + i + " " + nombre + " tenia "+ edad)
+
+    edad++;
+    
+}
+
+
